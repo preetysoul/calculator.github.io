ccf83555c9324b7e97e8d5cd612bbc62ab10dd05
mijash <118195783+preetysoul@users.noreply.github.com>
2025-11-09 16:23:42 +0545

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let btnText = e.target.innerHTML;
+
+        if (btnText === '=') {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch {
+                input.value = "Error";
+                string = "";
+            }
+        } else if (btnText === 'AC') {
+            string = "";
+            input.value = string;
+        } else if (btnText === 'DEL') {
+            string = string.slice(0, -1);
+            input.value = string;
+        } else {
+            string += btnText;
+            input.value = string;
+        }
+    });
+});
